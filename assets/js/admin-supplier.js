c7bcc81e7bc458475af5a90a035d62330849f474
AgungPN <agungpn33@gmail.com>
2024-03-23 09:28:09 +0700

    this will integrate with supplier, done all feature (CRUD, saerch, paginate)

---
+++ assets/js/admin-supplier.js
@@ -0,0 +1,161 @@
+const rootPath = require("electron-root-path").rootPath;
+const http = require(rootPath + "/utils/http");
+const createAlert = require(rootPath + "/utils/alert");
+const helpers = require(rootPath + "/utils/helpers");
+
+// Begin Logic to populate the data
+const sampleRow = document.getElementById("sample-row");
+const tableBody = document.querySelector("#data-table tbody");
+const filterData = new helpers.FilterData();
+
+function setTable() {
+  // Clear existing table rows
+  while (tableBody.firstChild) {
+    tableBody.removeChild(tableBody.firstChild);
+  }
+
+  http.client
+    .get(filterData.toQueryParams("/suppliers"))
+    .then((response) => {
+      filterData.setTotalPage(response.data.totalPages);
+      response.data.content.forEach((row) => {
+        const newRow = createNewRow(row);
+        tableBody.appendChild(newRow);
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+// call set table to populate the data and put it into the table
+setTable();
+
+function createNewRow(row) {
+  const newRow = sampleRow.cloneNode(true);
+  newRow.classList.remove("d-none");
+
+  newRow.querySelector(".name").innerHTML = row.name;
+  newRow.querySelector(".address").innerHTML = row.address;
+  newRow.querySelector(".phone").innerHTML = row.phone;
+  newRow.querySelector(".createdAt").innerHTML = row.createdAt;
+  newRow
+    .querySelector(".action button[data-edit-id]")
+    .setAttribute("data-edit-id", row.supplierId);
+  newRow
+    .querySelector(".action button[data-delete-id]")
+    .setAttribute("data-delete-id", row.supplierId);
+
+  return newRow;
+}
+
+// End Logic to populate the data
+
+// Create Supplier
+const formCreate = document.getElementById("formCreateSupplier");
+const placeModalCreateMessage = document.getElementById("modal-create-message");
+
+formCreate.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = formCreate.querySelector('input[name="name"]').value;
+  const phone = formCreate.querySelector('input[name="phone"]').value;
+  const address = formCreate.querySelector('textarea[name="address"]').value;
+
+  http.client
+    .post("suppliers", { name, phone, address })
+    .then((response) => {
+      const alertSuccess = createAlert("success", response.data.message);
+      placeModalCreateMessage.appendChild(alertSuccess);
+      setTable();
+    })
+    .catch((err) => {
+      helpers.showErrorMessages(err, placeModalCreateMessage);
+    });
+});
+// End Create Supplier
+
+const generalListMessages = document.getElementById("general-list-messages");
+
+const formEdit = document.getElementById("formEditSupplier");
+
+// handle edit button to change value form
+function handleEditView(e) {
+  const supplierId = e.getAttribute("data-edit-id");
+  http.client
+    .get(`suppliers/${supplierId}`)
+    .then((response) => {
+      const supplier = response.data.data;
+
+      formEdit.querySelector('input[name="name"]').value = supplier.name;
+      formEdit.querySelector('input[name="id"]').value = supplier.supplierId;
+      formEdit.querySelector('input[name="phone"]').value = supplier.phone;
+      formEdit.querySelector('textarea[name="address"]').innerHTML =
+        supplier.address;
+    })
+    .catch((err) => {
+      helpers.showErrorMessages(err, generalListMessages);
+    });
+}
+
+const placeModalEditMessage = document.getElementById("modal-edit-message");
+
+//handle submit update
+formEdit.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const id = formEdit.querySelector('input[name="id"]').value;
+  const name = formEdit.querySelector('input[name="name"]').value;
+  const phone = formEdit.querySelector('input[name="phone"]').value;
+  const address = formEdit.querySelector('textarea[name="address"]').value;
+
+  http.client
+    .put(`suppliers/${id}`, { name, phone, address })
+    .then((response) => {
+      const alertSuccess = createAlert("success", response.data.message);
+      placeModalEditMessage.appendChild(alertSuccess);
+      setTable();
+    })
+    .catch((err) => {
+      helpers.showErrorMessages(err, placeModalEditMessage);
+    });
+});
+
+// handle delete button
+function handleDelete(e) {
+  const isConfirmed = confirm("Are you sure you want to delete?");
+  if (!isConfirmed) return;
+
+  const supplierId = e.getAttribute("data-delete-id");
+  http.client
+    .delete(`suppliers/${supplierId}`)
+    .then((response) => {
+      console.log(response.data);
+      const alertSuccess = createAlert("success", response.data.message);
+      generalListMessages.appendChild(alertSuccess);
+      setTable();
+    })
+    .catch((err) => {
+      helpers.showErrorMessages(err, generalListMessages);
+    });
+}
+
+// Handling fitering (search, next, previous page)
+function handleSearch(e) {
+  filterData.setSearch(e.value);
+  setTable();
+}
+
+function handleNextPage(e) {
+  const isPageChanged = filterData.incrementPage();
+  if (isPageChanged) {
+    setTable();
+  }
+}
+
+function handlePrevPage(e) {
+  const isPageChanged = filterData.decremnetPage();
+  if (isPageChanged) {
+    setTable();
+  }
+}
