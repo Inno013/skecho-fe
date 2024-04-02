43e0948a2f407109c4bfd49ed5e6a1317eb75c5f
AgungPN <agungpn33@gmail.com>
2024-04-02 12:27:45 +0700

    add scrollable

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const createWindow = () => {
     },
   });
   win.webContents.openDevTools(); // helper only for development
-  win.loadFile("views/kasir/KasirPage.html");
+  win.loadFile("views/admin/products/Produk.html");
   // win.on("close", function (e) {
   //   const choice = dialog.showMessageBoxSync(this, {
   //     type: "question",
